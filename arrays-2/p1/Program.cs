8fffbb819d684e97677d2778962abdef0ef37622
jmatthewsc2k <78730700+jmatthewsc2k@users.noreply.github.com>
2021-03-01 11:49:24 +0000

    .

--- arrays-2/p1/Program.cs
+++ arrays-2/p1/Program.cs
@@ -6,7 +6,28 @@ namespace p1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] student = new string[5];
+
+            student[0] = "Molly";
+            student[1] = "Sabba";
+            student[2] = "Hatty";
+            student[3] = "Beth";
+            student[4] = "Jade";
+
+            //Outputting individual elements of the array
+            Console.WriteLine(student[0]);
+            Console.WriteLine(student[1]);
+            Console.WriteLine(student[2]);
+            Console.WriteLine(student[3]);
+            Console.WriteLine(student[4]);
+
+            //Using a loop
+            for(int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine(student[i]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
