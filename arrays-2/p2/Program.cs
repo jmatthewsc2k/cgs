81d6dbee1f3a448a3dc9be491135277f9e177a7b
jmatthewsc2k <78730700+jmatthewsc2k@users.noreply.github.com>
2021-03-01 11:58:35 +0000

    p2

---
+++ arrays-2/p2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+
+            numbers[0] = 5;
+            numbers[1] = 10;
+            numbers[2] = 15;
+
+            //Declare variable total and set to 0
+            int total = 0;
+
+            //Using a for loop you can add them together
+            for(int i =0; i < 3; i++)
+            {
+                total = total + numbers[i];
+            }
+
+            //Output the total
+            Console.WriteLine($"The numbers added together are: {total}");
+
+            Console.ReadLine();
+        }
+    }
+}
