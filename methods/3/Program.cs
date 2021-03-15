9e899a0ac8df3d3299cd75824078aa05a949a643
jmatthewsc2k <78730700+jmatthewsc2k@users.noreply.github.com>
2021-03-15 11:16:55 +0000

    methods3

---
+++ methods/3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            greeting("Allan",true);
+            Console.WriteLine("\r\n");
+            greeting("Bill",false);
+            Console.ReadLine();
+        }
+        
+        public static void greeting(string firstname,bool liked)
+        {
+            Console.WriteLine($"Hello, {firstname}");
+            if (liked == true)
+            {
+                Console.WriteLine("Nice to meet you! ");
+            }
+            else
+            {
+                Console.WriteLine("I don't like you - go away. ");
+            }
+        }
+    }
+}
