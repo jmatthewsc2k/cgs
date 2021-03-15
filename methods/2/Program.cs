ac1adfeb08e5f74481f070b89aa0bc1a9cc3df64
jmatthewsc2k <78730700+jmatthewsc2k@users.noreply.github.com>
2021-03-15 11:09:30 +0000

    methods2

---
+++ methods/2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            greeting("Alice");
+            greeting("Tayyiba");
+            Console.ReadLine();
+        }
+
+        public static void greeting(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+            Console.WriteLine("Pleased to meet you.");
+        }
+    }
+}
